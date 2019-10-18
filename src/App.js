6f3061b5d594222e17b1e69bb3f242181723527d
Per-Anders Karlsson <skidpa@users.noreply.github.com>
2019-10-18 10:00:36 +0200

    Revert "added login, restaurant, signUp, userUpdate forms and containers"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 import Sec from './Sec.js';
 import SearchBar from './SearchBar';
 import Container from './Container.js';
 
-import Login from './containers/Login';
-import SignUp from './containers/SignUp';
-import UserUpdate from './containers/UserUpdate';
-import NewRestaurant from './containers/NewRestaurant';
-
-import {BrowserRouter as Router, Switch, Route, Link}  from "react-router-dom";
-
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            role: 0
-        }
-    }
-
-    handleRole(newRole) {
-        this.setState({
-            role: newRole
-        });
-    }
-
 
     renderSection() {
         return (
@@ -69,58 +47,16 @@ import Switchclass from "./Switchclass";
 
 export default function App() {
     return (
-          <Router>
+        <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to ="/">Home</Link>
-                        </li>       
-                        <li>
-                            <Link to ="/login">Login</Link>
-                        </li>       
-                        <li>
-                            <Link to ="/signUp">Sign up</Link>
-                        </li>   
-                          <li>
-                            <Link to ="/newRestaurant">New Restaurant</Link>
-                        </li>  
-                    </ul>
-                </nav>
+                <Navclass/>
 
-                <Switch>
-                    <Route path="/login">
-                        <Login
-                            role={this.state.role}
-                            onRoleChanged={(role) => this.handleRole(role)}
-                        >
-                        </Login>
-                    </Route> 
-                    <Route path="/signUp">
-                        <SignUp
-                            rolee={this.state.role}
-                        >
-                        </SignUp>
-                    </Route>
-                     <Route path="/newRestaurant">
-                        <NewRestaurant>
-                        </NewRestaurant>
-                    </Route>
 
-                    <Route path="/">
-                      
-                      <div className="App">
-                          <SearchBar/>
-
-                          {this.renderMain()}
-                      
-                      </div>
-
-                    </Route>    
-                </Switch>
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. *///}
+                /*<Switchclass />
             </div>
-          </Router> 
-
+        </Router>
     );
 }*/
 
