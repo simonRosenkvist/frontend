40f2052d28573b5fa4801bea91111c280b46f3e1
Per-Anders Karlsson <karlsson.peranders@gmail.com>
2019-10-21 08:33:59 +0200

    uppdate restaurant button with non working state...

---
+++ src/containers/UpdateRestaurant.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+import NewRestaurantForm from "../components/NewRestaurantForm";
+import axios from "axios";
+
+class UpdateRestaurant extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            header: "Update Restaurant",
+            error: 'none',
+            restaurantId: 0,
+            name: '',
+            address: '',
+            description: '',
+            category: '',
+            usersId_fk: 0,
+        }
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        let apiUrl = 'http://restau-back.herokuapp.com/api/restaurant/update';
+        let restUpdate = {
+            "restaurantId": this.state.restaurantId,
+            "name": this.state.name,
+            "description": this.state.description,
+            "category": this.state.category,
+            "usersId_fk": this.state.usersId_fk
+        }
+        const parent = this;
+        console.log("before restau update");
+        console.log(restUpdate);
+        /*axios.post(apiUrl, restUpdate)
+            .then(function (response){
+                //do stuff
+        });*/
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    render(){
+        if(this.state.finished){
+            return (
+                <Redirect to="/"/>
+            );
+        }
+
+        return(
+            <NewRestaurantForm
+                header={this.state.header}
+                name={this.state.name}
+                address={this.state.address}
+                description={this.state.description}
+                category={this.state.category}
+                error={this.state.error}
+                onChange={this.onChange}
+                handleSubmit={(e) => this.handleSubmit(e)}
+            >
+            </NewRestaurantForm>
+        );
+    }
+} export default UpdateRestaurant;
